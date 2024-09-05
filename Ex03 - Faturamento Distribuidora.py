999cd72415fd17ca33f891b03346cce6cb8c4b78
Victor <victorgsg7272@gmail.com>
2024-09-04 23:37:47 -0300

    Criando Gerador de Dados fictícios onde os finais de semana devem ser valores 0

--- Ex03 - Faturamento Distribuidora.py
+++ Ex03 - Faturamento Distribuidora.py
@@ -1,4 +1,6 @@
 '''
+Para este exercício, pesquisei mais afubdo como obter dados fictícios e, descobri a biblioteca Faker().
+
 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
     • O menor valor de faturamento ocorrido em um dia do mês;
     • O maior valor de faturamento ocorrido em um dia do mês;
@@ -8,4 +10,10 @@ IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 '''
+import json
 
+# Abrindo o arquivo "distribuidora.json"
+with open('Ex03-Dados_Distribuidora.json', 'r') as arquivo:
+    dados = json.load(arquivo)
+
+print(dados['faturamento'].keys())
---
+++ Ex03-Gerador_de_Dados.py
@@ -0,0 +1,45 @@
+import json
+from faker import Faker
+from datetime import date
+from random import randint
+from calendar import monthrange
+
+
+fk = Faker('pt_BR')
+
+
+def gerar_dados_distribuidora(ano):
+    distribuidora = {
+        'nome': fk.company(),
+        'endereco': fk.address(),
+        'telefone': fk.phone_number(),
+        'cnpj': fk.cnpj(),
+        'faturamento': []
+    }
+
+    meses = ['Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outrubro', 'Novembro', 'Dezembro']
+
+    for mes in meses:
+        faturamento_mes = {}
+
+        for dia in range(1, monthrange(ano, meses.index(mes) + 1)[1] + 1):
+
+            # Data recebe dados do ano de 2024
+            data = date(ano, meses.index(mes) + 1, dia)
+
+            # Se a data for sábado(índice 5) ou domingo(índice 6)
+            valor = 0 if data.weekday() >= 5 else randint(0, 1000)
+
+            faturamento_mes[f"dia {dia}"] = valor
+
+        distribuidora['faturamento'].append({mes: faturamento_mes})
+
+    return distribuidora
+
+
+# Exemplo de uso, gerando dados para o ano de 2024
+dados_distribuidora = gerar_dados_distribuidora(2024)
+
+# Salvando em um arquivo JSON
+with open('distribuidora.json', 'w', encoding='utf-8') as f:
+    json.dump(dados_distribuidora, f, indent=4)
