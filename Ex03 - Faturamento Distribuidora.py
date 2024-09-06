24823172ac32ad3b101c67853e1b9ac31dc3d0cd
Victor <victorgsg7272@gmail.com>
2024-09-06 02:23:40 -0300

    Ex03 Finalizado e Pronto!

--- Ex03 - Faturamento Distribuidora.py
+++ Ex03 - Faturamento Distribuidora.py
@@ -1,5 +1,5 @@
 '''
-Para este exercício, pesquisei mais afubdo como obter dados fictícios e, descobri a biblioteca Faker().
+Para este exercício, pesquisei mais afundo como obter dados fictícios(já que não conseguia encontrar algum arquivo em JSON ou XML) e, descobri a biblioteca Faker().
 
 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
     • O menor valor de faturamento ocorrido em um dia do mês;
@@ -10,7 +10,13 @@ IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 '''
+from colorama import Fore
 import json
+
+
+# Cor
+ciano = Fore.CYAN
+resetar = Fore.RESET
 
 
 # Abrindo o arquivo "Ex03-Distribuidora.json"
@@ -19,33 +25,48 @@ with open('Ex03-Distribuidora.json', 'r') as arquivo:
 
 
 meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
 faturamento = []
+dias_acima_media = []
+maior_faturamento = []
+menor_faturamento = []
+media_meses = []
 
 
-for meses in dados['faturamento']:# Acessa "Meses : Dias" 
+for dados_mensais in dados['faturamento']: # Acessa "Meses : Dias" 
 
-    for dias in meses.values():# Acessa "Dias" em Meses
-        valor = 0
+    for dias in dados_mensais.values(): # Acessa "Dias" em Meses
+        fat_mensal = [] # Lista auxiliar
 
-        for valor_dia in dias.values():# Acessa "Valores" dos dias
-            
+        for valor_dia in dias.values(): # Acessa "Valores" dos dias
+
             if valor_dia == 0:
                 continue
             else:
-                valor += valor_dia
-
-        faturamento.append(valor)
-
-
-            
-    
-    # Extrair os valores de faturamento de todos os dias
-        # for dia, valor in dias.items():
-
+                fat_mensal.append(valor_dia)
         
 
+        media_mes = sum(fat_mensal) / len(fat_mensal) # Calcula a média mensal
+        media_alta = sum(1 for valor in fat_mensal if valor > media_mes) # Conta +1 para cada valor acima da média
+ 
+ 
+        maior_faturamento.append(max(fat_mensal)) # Maior faturamento
+        menor_faturamento.append(min(fat_mensal)) # Menor faturamento
+        dias_acima_media.append(media_alta) # Adiciona quantidade de dias acima da média
+        media_meses.append(f'{media_mes:.2f}') # Adiciona a média
 
 
-# valores = list(dias.values()) # Pega o Faturamento mensal de cada mês em forma de lista
-# print(sum(valores))
-# faturamento.append(dia)
+# =============================================================================
+# ============================================================ TELA DE MENSAGEM
+print(f'{ciano}{" ANÁLISE FATURAMENTO MENSAL ":=^115}{resetar}',end='\n\n')
+
+for i in range(len(meses)):
+    mes = f'{meses[i] + " ":-<13}'
+    menor_valor = f'{str(menor_faturamento[i]) + " ":-<6}'
+    maior_valor = f'{str(maior_faturamento[i]) + " ":-<7}'
+    acima_media = f'{str(dias_acima_media[i]):-<5}'
+    media = f'{str(media_meses[i]):<10}'
+
+    print(f'>>> {ciano}{mes}{resetar} Menor valor: {ciano}R$ {menor_valor}{resetar} Maior valor: {ciano}R$ {maior_valor}{resetar} Dias acima da média: {ciano}{acima_media}{resetar} Media mensal: {ciano}R$ {media}{resetar}')
+
+print()
