267778e298c70ce76ad639f3f1daebd54c13231e
Victor <victorgsg7272@gmail.com>
2024-09-06 02:57:15 -0300

    Ex05 Finalizado e Pronto!

--- Ex05 - Inverter Strings.py
+++ Ex05 - Inverter Strings.py
@@ -5,3 +5,9 @@ IMPORTANTE:
 a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 b) Evite usar funções prontas, como, por exemplo, reverse;
 '''
+frase = str(input('Digite alguma coisa: '))
+nome = f'!atartnoc eM'
+
+print(nome[::-1], '\n', frase[::-1].strip())
+
+
